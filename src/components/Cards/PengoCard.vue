146f6c5a267c09bbc7cf97a50fe2e7ca89fcd32d
Anmol Srivastava <anmol150srivastava@gmail.com>
2021-03-18 03:08:16 +0530

    pengocard

--- src/components/Cards/PengoCard.vue
+++ src/components/Cards/PengoCard.vue
@@ -1,54 +1,31 @@
 <template>
   <div>
     <card
-      type="pricing"
-      class="card-primary card-white"
-      footer-classes="text-center mb-3 mt-3"
+        type="pricing"
+        class="card-primary"
+        footer-classes="text-center mb-3 mt-3"
     >
-      <h1 class="card-title">pro</h1>
-      <img class="card-img" src="img/card-primary.png" alt="Image" />
-      <ul class="list-group">
-        <li class="list-group-item">300 messages</li>
-        <li class="list-group-item">150 emails</li>
-        <li class="list-group-item">24/7 Support</li>
-      </ul>
-      <div class="card-prices">
-        <h3 class="text-on-front"><span>$</span>95</h3>
-        <h5 class="text-on-back">95</h5>
-        <p class="plan">Professional plan</p>
-      </div>
-      <base-button slot="footer" round type="primary" class="btn-just-icon">
-        Get started
-      </base-button>
-    </card>
-    <card
-      type="pricing"
-      class="card-primary card-white"
-      footer-classes="text-center mb-3 mt-3">
-<!--      :style="{ border: '2px solid ' + this.getCardOutlineColor }"-->
-<!--    >-->
-<!--      <img class="card-img" :src="image" alt="Image" />-->
       <h1 class="card-title">{{ this.name }}</h1>
-      <ul class="list-group">
-        <li class="list-group-item">300 messages</li>
-        <li class="list-group-item">150 emails</li>
-        <li class="list-group-item">24/7 Support</li>
-      </ul>
-      <div class="card-prices">
-        <h3 class="text-on-front"><span>$</span>95</h3>
-        <h5 class="text-on-back">95</h5>
-        <p class="plan">Professional plan</p>
-      </div>
-      <base-button slot="footer" round type="primary" class="btn-just-icon">
-        Get started
-      </base-button>
+      <img class="card-img" src="img/card-primary.png" alt="Image" />
+      <img :src="image" style="display: block; position: relative!important; width: 60%; margin: auto;" alt="Image" />
+      <template v-if="type === 'market'">
+        <div class="card-prices" style="padding-top: 40px">
+          <h3 class="text-on-front"><span>$</span>95</h3>
+          <h5 class="text-on-back">95</h5>
+          <p class="plan">Professional plan</p>
+        </div>
+        <base-button slot="footer" round type="primary" class="btn-just-icon">
+          Get started
+        </base-button>
+      </template>
     </card>
   </div>
 </template>
 <script>
-import Card from "@/components/Cards/Card";
+
 export default {
-  name: 'card',
+  name: 'pengo-card',
+  // props: ['name', 'image', 'cost', 'grade'],
   props: {
     name: {
       type: String,
@@ -61,11 +38,15 @@ export default {
     cost: {
       type: String,
       description: 'Listed price',
-      default: '0',
     },
     grade: {
       type: String,
       description: 'Card type'
+    },
+    type: {
+      type: String,
+      description: 'Card type',
+      default: '',
     },
   },
   // computed: {
