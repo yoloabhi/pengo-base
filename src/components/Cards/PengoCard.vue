f65f331a28c00a70c56b72b7b487263154f07209
yoloabhi <abhirup_cool2@yahoo.com>
2021-03-17 15:33:48 +0530

    Pengo card layout added

---
+++ src/components/Cards/PengoCard.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <card
+      type="pricing"
+      class="card-primary card-white"
+      footer-classes="text-center mb-3 mt-3"
+      :style="{ border: '2px solid ' + this.getCardOutlineColor }"
+    >
+      <img class="card-img" :src="image" alt="Image" />
+      <h1 class="card-title">{{ this.name }}</h1>
+      <ul class="list-group">
+        <li class="list-group-item">300 messages</li>
+        <li class="list-group-item">150 emails</li>
+        <li class="list-group-item">24/7 Support</li>
+      </ul>
+      <div class="card-prices">
+        <h3 class="text-on-front"><span>$</span>95</h3>
+        <h5 class="text-on-back">95</h5>
+        <p class="plan">Professional plan</p>
+      </div>
+      <base-button slot="footer" round type="primary" class="btn-just-icon">
+        Get started
+      </base-button>
+    </card>
+  </div>
+</template>
+<script>
+import Card from "@/components/Cards/Card";
+export default {
+  name: 'card',
+  props: {
+    name: {
+      type: String,
+      description: 'Card title'
+    },
+    image: {
+      type: String,
+      description: 'Card Image'
+    },
+    cost: {
+      type: String,
+      description: 'Listed price'
+    },
+    grade: {
+      type: String,
+      description: 'Card type'
+    },
+  },
+  computed: {
+    getCardOutlineColor () {
+      if (this.grade === 'general') {
+        return 'gold';
+      } else if (this.grade === 'lieutenant') {
+        return 'red';
+      } else
+      if (this.grade === 'captain') {
+        return 'violet';
+      }
+      if (this.grade === 'major') {
+        return 'blue';
+      }
+      else {
+        return 'black';
+      }
+    }
+  },
+  mounted() {
+    console.log('YOII')
+  }
+};
+</script>
+<style></style>
